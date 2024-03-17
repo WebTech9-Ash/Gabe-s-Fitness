dc779d8c60671c0378cac9a6bdc7d2984b4eaacc
WebTech9-Ash <159192419+WebTech9-Ash@users.noreply.github.com>
2024-03-17 15:50:01 +0000

    Add files via upload

---
+++ action/gym_membership_action.php
@@ -0,0 +1,42 @@
+<?php
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    require_once 'connection.php';
+
+    $memberId = $_POST['member-id'];
+    $memberEmail = $_POST['member-email'];
+    $memberPassword = $_POST['member-password']; 
+    $gymId = $_POST['gym-select'];
+    $membershipPlan = $_POST['member-plan'];
+
+    $sql = "SELECT UserID FROM Users WHERE Email = '$memberEmail' AND Passwd = '$memberPassword'";
+    $result = $conn->query($sql);
+
+    if ($result->num_rows > 0) {
+        $joinDate = date("Y-m-d"); 
+        
+      
+        $expiryDate = date('Y-m-d', strtotime("+1 year")); 
+        
+        
+        $memberId = mysqli_real_escape_string($conn, $memberId);
+        $gymId = mysqli_real_escape_string($conn, $gymId);
+        $membershipPlan = mysqli_real_escape_string($conn, $membershipPlan);
+
+        $query = "INSERT INTO Memberships (UserID, GymID, JoinDate, MembershipType, ExpiryDate) VALUES ('$memberId', '$gymId', '$joinDate', '$membershipPlan', '$expiryDate')";
+
+        $result = mysqli_query($conn, $query);
+
+        if ($result) {
+            echo "Membership registered successfully.";
+        } else {
+            echo "Failed to register membership: " . mysqli_error($conn);
+        }
+    } else {
+        echo "Invalid email or password.";
+    }
+
+    mysqli_close($conn);
+} else {
+    echo "Form data not submitted.";
+}
