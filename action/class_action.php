a5934e4153e6f35af49cbf9fc315bd2b4ed234e5
SKArABS <123462279+SKArABS@users.noreply.github.com>
2024-03-17 21:53:19 +0000

    Update class_action.php

--- action/class_action.php
+++ action/class_action.php
@@ -1,4 +1,5 @@
 <?php
+session_start();
 include '../setting/connection.php';
 //Joining a class
 if (isset($_POST['join-class'])) {
@@ -6,9 +7,7 @@ if (isset($_POST['join-class'])) {
     $sessionId = $_POST['session-id']; 
     $attendanceTime = $_POST['attendance-time'];
 
-
-    $userId = $_SESSION['userId']; 
-    $query = "INSERT INTO ClassAttendees (SessionID, UserID, AttendanceDateTime) VALUES ('$sessionId', '$userId', '$attendanceTime')";
+    $query = "INSERT INTO ClassAttendees (SessionID, UserID, AttendanceDateTime) VALUES ('" . $_SESSION['userId'] . "', '$userId', '$attendanceTime')";
     $result = mysqli_query($conn, $query);
     if ($result) {
         echo "Class Joined!";
@@ -23,7 +22,7 @@ if (isset($_POST['leave-class'])) {
 
     $sessionId = $_POST['session-id']; 
 
-    $userId = $_SESSION['userId']; 
+    $userId = $_SESSION['userId'] 
 
     $query = "DELETE FROM ClassAttendees WHERE UserID = '$userId' AND SessionID = '$sessionId'";
     $result = mysqli_query($conn, $query);
