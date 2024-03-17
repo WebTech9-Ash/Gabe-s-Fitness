0fd6544bc00c80a3b7449d1ecf202cb84230c6b7
Beryl Koram <92120634+berylkor@users.noreply.github.com>
2024-03-17 23:38:07 +0000

    Update landingpage.php

--- view/landingpage.php
+++ view/landingpage.php
@@ -12,7 +12,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 
 </head>
-<body class="light-mode">
+<body class="dark-mode">
 
     <header class = navbar> 
        <img src="../assets/logo.png" id="logo" alt="logo">
@@ -86,6 +86,7 @@
     <script src="../js/index.js">
         
     </script>
-
+    <script type="module" src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.esm.js"></script>
+<script nomodule src="https://unpkg.com/ionicons@7.1.0/dist/ionicons/ionicons.js"></script>
 </body>
 </html>
