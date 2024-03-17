26ed37936af49058f4ec275748ed2d99844fe079
Beryl Koram <92120634+berylkor@users.noreply.github.com>
2024-03-17 20:57:00 +0000

    Update add_class_session.php

--- view/add_class_session.php
+++ view/add_class_session.php
@@ -22,7 +22,7 @@
     <nav>
         <ul>
             <li><a href="../view/homepage.php">Home</a></li>
-            <li><a href="#">About Us</a></li>
+            <li><a href="../view/about_view.php">About Us</a></li>
             <li><a href="../view/dashboard_view.php">Dashboard</a></li>
             <li><a href="../view/profile.php">Profile</a></li>
             <li><a href="../view/ManTrack.php">Tracker</a></li>
