60b39249eb446e0ff08efd38d585afef68e419ce
SKArABS <123462279+SKArABS@users.noreply.github.com>
2024-03-17 21:30:10 +0000

    Update gym_membership_action.php

--- action/gym_membership_action.php
+++ action/gym_membership_action.php
@@ -1,42 +1,27 @@
 <?php
-
+session_start();
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     include '../setting/connection.php';
-
-    $memberEmail = $_POST['member-email'];
-    $pass = $_POST['member-password']; 
-    $memberPassword = password_hash($pass, PASSWORD_DEFAULT);
-    $gymId = $_POST['gym-select'];
-
-    echo "Member Email: " . $memberEmail . "<br>";
-    echo "Hashed Password: " . $memberPassword . "<br>";
-    echo "Gym ID: " . $gymId . "<br>";
     
-    $query = "SELECT UserID FROM Users WHERE Email = '$memberEmail' AND Passwd = '$memberPassword'";
-    $result = $conn->query($query);
-
-    if ($result->num_rows > 0) {
-        $joinDate = date("Y-m-d"); 
+    $gymId = $_POST['gym-select'];
+    
+    $joinDate = date("Y-m-d"); 
         
       
-        $expiryDate = date('Y-m-d', strtotime("+1 year")); 
+    $expiryDate = date('Y-m-d', strtotime("+1 year")); 
         
-        
-        $memberId = mysqli_real_escape_string($conn, $memberId);
-        $gymId = mysqli_real_escape_string($conn, $gymId);
-        //$membershipPlan = mysqli_real_escape_string($conn, $membershipPlan);
+    $gymId = mysqli_real_escape_string($conn, $gymId);
+    //$membershipPlan = mysqli_real_escape_string($conn, $membershipPlan);
 
-        $query = "INSERT INTO Memberships (UserID, GymID, JoinDate, ExpiryDate) VALUES ('$result', '$gymId', '$joinDate', '$expiryDate')";
+    $query = "INSERT INTO Memberships (UserID, GymID, JoinDate, ExpiryDate) VALUES ('$_SESSION['userId']', '$gymId', '$joinDate', '$expiryDate')";
 
-        $result = mysqli_query($conn, $query);
+    $result = mysqli_query($conn, $query);
 
-        if ($result) {
+    if ($result) {
             echo "Membership registered successfully.";
         } else {
             echo "Failed to register membership: " . mysqli_error($conn);
         }
-    } else {
-        echo "Invalid email or password.";
     }
 
     mysqli_close($conn);
