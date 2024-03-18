525d64fbdf64f6d0a7e91e1a24e126194eb96203
SKArABS <123462279+SKArABS@users.noreply.github.com>
2024-03-18 13:06:02 +0000

    Update gym_membership_action.php

--- action/gym_membership_action.php
+++ action/gym_membership_action.php
@@ -5,7 +5,18 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // Set the 'userId' session variable
     include '../setting/connection.php';
     
-    $gymId = $_POST['gym-select'];
+    if(isset($_POST['gym-select'])) {
+        $gymId = $_POST['gym-select'];
+    } else {
+?>
+        <script>
+            alert("Please select a gym.");
+            // Redirect the user back to the previous page
+            window.history.back();
+        </script>
+<?php
+        exit(); // Stop further execution
+    }
     
     $joinDate = date("Y-m-d");
     $expiryDate = date('Y-m-d', strtotime("+1 year"));
