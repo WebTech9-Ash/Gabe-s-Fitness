f23af1f6a0a79bf6e632ba17c1d241e105c5776d
EddyKubwimana <eddy.kubwimana@ashesi.edu.gh>
2024-05-31 20:01:22 +0000

    updated the connexion file

--- setting/connection.php
+++ setting/connection.php
@@ -1,44 +1,25 @@
+
 <?php
   $db_host = '127.0.0.1';
-  $db_user = 'GABES2025';
-  $db_password = 'Team9';
+  $db_user = 'root';
+  $db_password = 'root';
   $db_db = 'GABES2025';
+  $db_port = 8889;
+
   $conn = new mysqli(
     $db_host,
     $db_user,
     $db_password,
     $db_db,
-  );
-
-  if ($conn->connect_error) {
-    echo "The error has happened";
-    echo 'Errno: '.$conn->connect_errno;
-    echo '<br >';
-    echo 'Error: '.$conn->connect_error;
-   
-  }
-
-/*
-<?php
-$db_host = 'localhost';
-  $db_user = 'root';
-  $db_password = 'root';
-  $db_db = 'GABES2025';
- 
-  $conn = @new mysqli(
-    $db_host,
-    $db_user,
-    $db_password,
-    $db_db
+	$db_port
   );
 	
   if ($conn->connect_error) {
     echo 'Errno: '.$conn->connect_errno;
-    echo '<br>';
+    echo '<br >';
     echo 'Error: '.$conn->connect_error;
     exit();
   }
-?>
-'''
 
-*/
+?>
+
